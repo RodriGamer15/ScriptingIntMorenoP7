85d1fe425a582895dcc6df17c518bd3c6b5011b5
Rodrigo Moreno <750694@student.dusd.net>
2025-02-12 14:14:34 -0800

    Added method overloading
    I added 2 scripts for method overloading.

---
+++ IntermediateScriptingMoreno/Assets/Scripts/MethodScripts/SomeOtherClass.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SomeOtherClass : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        SomeClass myClass = new SomeClass();
+
+        //The specific Add method called will depend on
+        //the argument passed in.
+        myClass.Add(1, 2);
+        myClass.Add("Hello ", "World");
+    }
+}
